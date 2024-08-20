6fa3499fdb16249222575881f6dc0c47844996d0
chetan.patel <chetan.patel@LPCL-PF3MMYB6.espire.com>
2024-08-20 20:28:06 +0530

    Screenshot implementation

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,5 +1,10 @@
 package stepDefinitions;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -9,24 +14,42 @@ import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import otherResources.TestContext;
+import pageObjects.EproLoginPage;
 
-public class Hooks {
+public class Hooks{
 
-	/*
-	 * TestContext testContext;
-	 * 
-	 * public Hooks(TestContext tstContext) { testContext = tstContext; }
-	 * 
-	 * @Before public void BeforeSteps() {
-	 * 
-	 * 
-	 * }
-	 * 
-	 * 
-	 * 
-	 * @After public void AfterSteps() {
-	 * testContext.getWebDriverManager().closeDriver(); }
-	 */
+	TestContext testContext;
+	WebDriver driver ;
+
+	public Hooks(TestContext tstContext) {
+		testContext = tstContext;
+		driver = TestContext.getWebDriverManager().getDriver();
+
+	}
+
+	@Before
+	public void BeforeSteps() {
+		// driver = testContext.getWebDriverManager().getDriver(); // Ensure driver is initialized here
+	}
+
+
+
+	@AfterStep 
+	public void takeScreenshot(Scenario scenario) {
+		//   WebDriver driver = testContext.getWebDriverManager().getDriver(); // Use the instance from TestContext
+		try {
+			if (driver != null) {
+				final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+				scenario.attach(screenshot, "image/png", "image");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
+
+
+//  testContext.getWebDriverManager().closeDriver(); }
+
 
 
--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -1,26 +1,39 @@
 package testrunner;
 
+import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import otherResources.TestContext;
 
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
        //plugin = { "pretty", "html:target/cucumber-reports.html" },
-		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+		plugin = { "summary","pretty", "html:target/cucumber-reports.html",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"json:target/cucumber-reports"
+				},
       // this needs to be localized
-		features = "C:\\Users\\gaurav.shukla.ESPIRE\\git\\CucumberEspire\\src\\test\\resources\\Stories\\Epro.feature"
+		features = "C:\\Users\\chetan.patel\\Epro_Workspace\\Epro\\CucumberEspire\\src\\test\\resources\\Stories\\Epro.feature"
         ,glue={"stepDefinitions"}
         ,dryRun = false
-        ,monochrome = false
+        ,monochrome = true
         ,tags = "@Scenario1"
 )
 
 public class TestRunner {
 	
-	// after giving a tag @Before we can add any logic that we wish to execute before test case execution// same as @After
+	TestContext testcontext;
+	
+	public TestRunner() {
+			
+	}
+	
 
-}
+
+	
+
+	}
     //Sample comment  This comment is added in testrunner file. 
 //Sample comment  This comment is added in testrunner file. 
