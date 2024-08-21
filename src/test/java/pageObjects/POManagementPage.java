e0718dbe85910e791a95a0c273ff3cbbfa41b714
rahul <rahul@LPCL-PG033Y4F.espire.com>
2024-08-21 22:08:11 +0530

    Scenario_2_commit

--- src/test/java/pageObjects/POManagementPage.java
+++ src/test/java/pageObjects/POManagementPage.java
@@ -1,135 +1,127 @@
 package pageObjects;
-
+ 
 import java.util.concurrent.TimeUnit;
-
+ 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+ 
 import CommmonUtils.BaseAction;
-
+ 
 public class POManagementPage {
-	
-	
-	public WebDriver driver;
-	public WebDriverWait wait;
-	public JavascriptExecutor js;
-	public BaseAction ba;
-	public int rowNum;
-	
-
-	private final By element5 = By.xpath("//img[@src='assets/images/assign-suppliers.svg']");
-	private final By element = By.xpath("//div[@id='mat-select-value-13']");
-
+ 
+	private WebDriver driver;
+	private WebDriverWait wait;
+	private JavascriptExecutor js;
+	private BaseAction ba;
+	private int rowNum;
+ 
 	public POManagementPage(WebDriver driver) {
 		this.driver = driver;
 		this.js = (JavascriptExecutor) driver;
-		 this.wait = new WebDriverWait(driver,java.time.Duration.ofSeconds(20));
-		 ba = new BaseAction(driver);
+		this.wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(20));
+		ba = new BaseAction(driver);
 	}
-	
-	public void goToFinance () throws InterruptedException {
-		 // click on supporting docs link
-		   String pdfName = null;
-		   String[] act;
+ 
+	public void goToFinance() throws InterruptedException {
+		// click on supporting docs link
+		String pdfName = null;
+		String[] act;
+ 
+		ba.retryMechanism(driver, driver.findElement(By.xpath("//*[@id='mat-tab-label-2-2']")));
+ 
+		// ba.retryMechanism(driver, supportdoc);
+		// get the pdf PO name from table
+ 
+		for (int i = 0; i < 3; i++) {
+			try {
+				pdfName = ba.handleWebTable("//*[@role='table']/tbody/tr", "Purchase Order", 2, "getText");// hard coded
+																											// value
+ 
+				break;
+			} catch (StaleElementReferenceException e) {
+				e.printStackTrace();
+			}
+ 
+		} // end of for loop
+		act = pdfName.split("_");
+		System.out.println("split pdf name = " + act[0]);
+		// ePO000898-LEADGB_V3.pdf extract : ePO000898-LEADGB
+ 
+		// hover to select finance
+ 
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='sideNav']"))).click();
+		// click on finance
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()=' Finance ']"))).click();
+		// click on PO Management
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()=' PO Management']"))).click();
 		
-	       ba.retryMechanism(driver, driver.findElement(By.xpath("//*[@id='mat-tab-label-2-2']")));
-	       
-	       // get the pdf PO name from table
-     
-	       for (int i = 0; i < 3; i++) {
-	    	   try {
-	    		   pdfName = ba.handleWebTable("//*[@role='table']/tbody/tr","Purchase Order" , 2,"getText");// hard coded value
-	    		   
-	    		   break;}
-	    	   catch (StaleElementReferenceException e) {
-	    		   e.printStackTrace();
-	    	   }
-
-	    	    } // end of for loop
-	       act = pdfName.split("_");
-	       System.out.println("split pdf name = "+act[0]);
-	       //ePO000898-LEADGB_V3.pdf extract : ePO000898-LEADGB
-	      
-	
-	
-	
-		 //hover to select finance
-	       
-	       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='sideNav']"))).click();
-	     //  ba.retryMechanism(driver, driver.findElement(By.xpath("//*[@id='sideNav']")));
-	       //click on finance
-	       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()=' Finance ']"))).click();
-	     //  ba.retryMechanism(driver, driver.findElement(By.xpath("//span[text()=' Finance ']")));
-	       //click on PO Management
-	       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()=' PO Management']"))).click();
-	     //  ba.retryMechanism(driver, driver.findElement(By.xpath("//a[text()=' PO Management']")));
-
-	       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Search..']"))).clear();
-
-			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Search..']")))
-					.sendKeys(act[0]);
-
-			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Search..']"))).click();
-
-			
-	       for (int i = 0; i < 3; i++) {
-	    	   try {
-	    		  ba.handleWebTable("//*[@role='table']/tbody/tr", act[0], 1, "clickItem" ); // hard coded value
-	    		  break;}
-	    	   catch (StaleElementReferenceException e) {
-	    		   e.printStackTrace();
-	    	   }
-
-	    	    } // end of for loop
-	}   
-	
-	public void uploadPOD () throws InterruptedException {
-	       // code to be analysed ///////////////////////////////////////////////////////////////////////////////
-	       
-	       int rowNum1 = ba.getMatchRowNum("//*[@role='table']/tbody/tr", "UT01673-001", 2, "getRowNum");
-	       System.out.println("rownum 2 in PO section ="+rowNum1);
-	       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@role='table']/tbody/tr["+rowNum1+"]/td/button//img[@src='assets/images/attach_user_guide.svg']"))).click();
-	       Thread.sleep(3000);
-	       // select the file to be uploaded send keys to below weblement
-	     //*[@id='supportingDocumentFileUpload']
-	       driver.findElement(By.xpath("//*[@id='supportingDocumentFileUpload']")).sendKeys("C:\\Users\\rahul\\Downloads\\ePO000895-LEADGB_V1.pdf");
-	       // enter comments inthe text area
-	     //*[@id='supportingDocumentDescription']
-	       driver.findElement(By.xpath("//*[@id='supportingDocumentDescription']")).sendKeys("Coments");
-	       ba.retryMechanism(driver, driver.findElement(By.xpath("(//*[@role='combobox'])[2]")));
-	       
-	     
-	       // select pod option
-	       ba.retryMechanism(driver, driver.findElement(By.xpath("//span[text()=' Proof Of Delivery']")));
-	       
-	       // click the save button
-	       ba.retryMechanism(driver, driver.findElement(By.xpath("//button[@type='submit']")));
-	    
+ 
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Search..']"))).clear();
+ 
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Search..']")))
+				.sendKeys(act[0]);
+ 
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Search..']"))).click();
+ 
+		for (int i = 0; i < 3; i++) {
+			try {
+				ba.handleWebTable("//*[@role='table']/tbody/tr", act[0], 1, "clickItem"); // hard coded value
+				break;
+			} catch (StaleElementReferenceException e) {
+				e.printStackTrace();
+			}
+ 
+		} // end of for loop
 	}
-	
-	public void validateReciept () throws InterruptedException {
-	       // click on Receipt tab
-	       ba.retryMechanism(driver, driver.findElement(By.xpath("//*[@class='mat-tab-header']//div[text()='Receipt']")));
-	       
-	       // select the checkbox on receipt tab
-	       ba.handleWebTable("//*[@role='table']/tbody/tr", "UT01673-001", 2, "clickItem");  // hard coded value
-	       
-	       // click the receipt action
-	       ba.handleWebTable("//*[@role='table']/tbody/tr", "UT01673-001", 14, "clickItem");  // hard coded value
-	       
-	       // click on Close button on the popup
-	       ba.retryMechanism(driver, driver.findElement(By.xpath("//button/span[text()='Close']")));
-	      
-	       
-	             //get data from table
+ 
+	public void uploadPOD(String indexID) throws InterruptedException {
+		// code to be analysed
 
-	       //Scenario -2 completed
-	      
-	  }
+        String user_dir = System.getProperty("user.dir");
+		int rowNum1 = ba.getMatchRowNum("//*[@role='table']/tbody/tr", indexID, 2, "getRowNum");
+		System.out.println("rownum 2 in PO section =" + rowNum1);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@role='table']/tbody/tr[" + rowNum1
+				+ "]/td/button//img[@src='assets/images/attach_user_guide.svg']"))).click();
+		Thread.sleep(3000);
+		// select the file to be uploaded send keys to below weblement
+		
+		driver.findElement(By.xpath("//*[@id='supportingDocumentFileUpload']"))
+				.sendKeys(user_dir+"\\src\\test\\resources\\Upload_Files\\ePO000967-LEADGB_V1.pdf");
+		// enter comments inthe text area
+		driver.findElement(By.xpath("//*[@id='supportingDocumentDescription']")).sendKeys("Coments");
+		ba.retryMechanism(driver, driver.findElement(By.xpath("(//*[@role='combobox'])[2]")));
+ 
+		// select pod option
+		ba.retryMechanism(driver, driver.findElement(By.xpath("//span[text()=' Proof Of Delivery']")));
+ 
+		// click the save button
+		ba.retryMechanism(driver, driver.findElement(By.xpath("//button[@type='submit']")));
+ 
 	}
-
+ 
+	public void validateReciept(String indexID) throws InterruptedException {
+		// click on Receipt tab
+		ba.retryMechanism(driver, driver.findElement(By.xpath("//*[@class='mat-tab-header']//div[text()='Receipt']")));
+ 
+		// select the checkbox on receipt tab
+		ba.handleWebTable("//*[@role='table']/tbody/tr", indexID, 2, "clickItem"); // hard coded value
+ 
+		// click the receipt action
+		ba.handleWebTable("//*[@role='table']/tbody/tr", indexID, 14, "clickItem"); // hard coded value
+ 
+		// click on Close button on the popup
+		ba.retryMechanism(driver, driver.findElement(By.xpath("//button/span[text()='Close']")));
+ 
+		// get data from table
+ 
+		// Scenario -2 completed
+ 
+	}
+}
 
--- src/test/java/stepDefinitions/StepDef_Scenario2.java
+++ src/test/java/stepDefinitions/StepDef_Scenario2.java
@@ -1,7 +1,7 @@
 package stepDefinitions;
-
+ 
 import org.apache.poi.sl.usermodel.TextRun.TextCap;
-
+ 
 import TestResourceManager.WebDrivermanager;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -18,89 +18,82 @@ import pageObjects.ManagePricePage;
 import pageObjects.POManagementPage;
 import pageObjects.QuoteManagementPage;
 import pageObjects.SubmitSupplierPricePage;
-
+ 
 public class StepDef_Scenario2 {
-	
+ 
 	TestContext testContext;
-    EproLoginPage eprologin;
-    EproHomePage eprohome;
-    EproCampaignPage eprocamppage;
-    EproCreateCampaignPage eprocreatecamp;	
+	EproLoginPage eprologin;
+	EproHomePage eprohome;
+	EproCampaignPage eprocamppage;
+	EproCreateCampaignPage eprocreatecamp;
 	AddCampaignItemPage addCampaignItem;
 	EditCampaignItemPage editCampPage;
 	SubmitSupplierPricePage submitSupplier;
 	ManagePricePage managePrice;
-    QuoteManagementPage quoteManage;
-    ManageCampaignPage manageCampPage;
-    WebDrivermanager webdrivermanager;
-    POManagementPage poManagementPage;
-    
-    
-	
-	
-	 public StepDef_Scenario2(TestContext tstContext){
-	        testContext = tstContext;
-	      
-	        eprologin = testContext.getPageObjectManager().getEproLoginPage();
-	        eprohome =  testContext.getPageObjectManager().getEproHomePage();
-	        eprocamppage =  testContext.getPageObjectManager().getEproCampaignPage();
-	        eprocreatecamp =  testContext.getPageObjectManager().getEproCreateCampaignPage();
-	        addCampaignItem = testContext.getPageObjectManager().getAddCampaignItemPage();
-	        editCampPage = testContext.getPageObjectManager().getEditCampaignItemPage();
-	        submitSupplier = testContext.getPageObjectManager().getSubmitSpplierPricePage();
-	        managePrice = testContext.getPageObjectManager().getManagePricePage();
-	        manageCampPage = testContext.getPageObjectManager().getManageCampaignPage();
-	        quoteManage = testContext.getPageObjectManager().getQuoteManagementPage();
-	        poManagementPage = testContext.getPageObjectManager().getPOManagementPage();
-	        webdrivermanager = testContext.getWebDriverManager();
-	    }
-	 
-	
-
-	@And("On Campaign Item Page verify Status as Quote Accepted after Accepting the quote")
-	public void on_campaign_item_page_verify_status_as_quote_accepted_after_accepting_the_quote() throws InterruptedException {
-	    eprohome.ClkCampaign();
-	  // String campID = testContext.Hmap.get("CampaignId");
-	  // System.out.println(campID);
-	   eprocamppage.clickOnCampaignId("UT01673");
+	QuoteManagementPage quoteManage;
+	ManageCampaignPage manageCampPage;
+	WebDrivermanager webdrivermanager;
+	POManagementPage poManagementPage;
+ 
+	public StepDef_Scenario2(TestContext tstContext) {
+		testContext = tstContext;
+ 
+		eprologin = testContext.getPageObjectManager().getEproLoginPage();
+		eprohome = testContext.getPageObjectManager().getEproHomePage();
+		eprocamppage = testContext.getPageObjectManager().getEproCampaignPage();
+		eprocreatecamp = testContext.getPageObjectManager().getEproCreateCampaignPage();
+		addCampaignItem = testContext.getPageObjectManager().getAddCampaignItemPage();
+		editCampPage = testContext.getPageObjectManager().getEditCampaignItemPage();
+		submitSupplier = testContext.getPageObjectManager().getSubmitSpplierPricePage();
+		managePrice = testContext.getPageObjectManager().getManagePricePage();
+		manageCampPage = testContext.getPageObjectManager().getManageCampaignPage();
+		quoteManage = testContext.getPageObjectManager().getQuoteManagementPage();
+		poManagementPage = testContext.getPageObjectManager().getPOManagementPage();
+		webdrivermanager = testContext.getWebDriverManager();
 	}
-
-	@And("User will Send and the create PO")
-	public void user_will_send_and_the_create_po() throws InterruptedException {
-	    eprocamppage.clickOnSendPO();
-	    eprocamppage.clickOnCreatePO();
+ 
+ 
+	@And("On Campaign Item Page verify Status as Quote Accepted and User will Send and the create PO")
+	public void On_Campaign_Item_Page_verify_Status_as_Quote_Accepted_and_user_will_send_and_the_create_po() throws InterruptedException {
+		eprohome.ClkCampaign();
+		eprocamppage.clickOnCampID("Quote Accepted");
+		eprocamppage.SendAndCreatePO();
+		String indCampID = eprocamppage.getIndexValue();
+		testContext.Hmap.put("Index ID", indCampID);
+ 
 	}
-
+ 
 	@Then("Verify Status as PO Created after creating the PO on Campaign Item Page")
 	public void verify_status_as_po_created_after_creating_the_po_on_campaign_item_page() {
-	    System.out.println("status verified as po created");
+		System.out.println("status verified as po created");  // do assertion and verify
 	}
-
+ 
 	@And("User will navigate to Finance page to create receipt")
 	public void user_will_navigate_to_finance_page_to_create_receipt() throws InterruptedException {
-	   poManagementPage.goToFinance();
+		poManagementPage.goToFinance();
 	}
-
+ 
 	@Then("User will upload POD document")
 	public void user_will_upload_pod_document() throws InterruptedException {
-	    poManagementPage.uploadPOD();
+ 
+		poManagementPage.uploadPOD(testContext.Hmap.get("Index ID"));   //add underscore
+		
 	}
-
+ 
 	@And("Verify Has POD column status with green tick")
 	public void verify_has_pod_column_status_with_green_tick() {
-	    System.out.println("pod status verified, green tick enabled");
+		System.out.println("pod status verified, green tick enabled");  // need assertion
 	}
-
+ 
 	@And("User navigate to the Receipt Tab and click on required Checkbox")
 	public void user_navigate_to_the_receipt_tab_and_click_on_required_checkbox() throws InterruptedException {
-	    poManagementPage.validateReciept();
+		poManagementPage.validateReciept(testContext.Hmap.get("Index ID"));
 	}
-
+ 
 	@Then("Verify Receipted column status with green tick")
 	public void verify_receipted_column_status_with_green_tick() {
-	    System.out.println("Receipt status verified");
-	 //   webdrivermanager.closeDriver();
+		System.out.println("Receipt status verified");
+		// webdrivermanager.closeDriver();
 	}
-
-
+ 
 }
