ee47529cecb10276a7ec3c58292b44a81d5734cc
chetan.patel <chetan.patel@paragon-qa-autopoc.espire.com>
2024-09-12 16:37:57 +0530

    scenario 11 failed test logging

--- src/test/java/stepDefinitions/StepDef_Scenario11.java
+++ src/test/java/stepDefinitions/StepDef_Scenario11.java
@@ -1,5 +1,6 @@
 package stepDefinitions;
 
+import CommmonUtils.ExcelUtil;
 import TestResourceManager.WebDrivermanager;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
@@ -9,14 +10,15 @@ import pageObjects.SalesInvoicePage;
 import pageObjects.SalesOrderPage;
 
 public class StepDef_Scenario11 {
-	
+
 	TestContext testContext;
 
 	WebDrivermanager webdrivermanager;
 	SalesOrderPage salesOrderPg;
 	SalesInvoicePage salesInvoicePg;
 	POManagementPage poManagementPage;
-	
+	String methodName;
+
 	public StepDef_Scenario11(TestContext tstContext) {
 		testContext = tstContext;
 
@@ -24,59 +26,120 @@ public class StepDef_Scenario11 {
 		salesInvoicePg = testContext.getPageObjectManager().getSalesInvoicePage();
 		poManagementPage = testContext.getPageObjectManager().getPOManagementPage();
 	}
-	
-	
+
+
 	@And("User navigate to the Receipt Tab and click on required Checkbox for scenario11")
 	public void user_navigate_to_the_receipt_tab_and_click_on_required_checkbox_for_scenario11() throws InterruptedException {
-		
-		String UTnumber = poManagementPage.validateRecieptForCancelSOLine(testContext.Hmap.get("Index ID"));
-            System.out.println("UTNumer from scenario 2 = "+UTnumber);
-		testContext.Hmap.put("UTNumber", UTnumber);
+
+		methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+
+		try {
+			String UTnumber = poManagementPage.validateRecieptForCancelSOLine(testContext.Hmap.get("Index ID"));
+			System.out.println("UTNumer from scenario 2 = "+UTnumber);
+			testContext.Hmap.put("UTNumber", UTnumber);
+		}
+		catch(Exception e) {
+			System.out.println("In Catch Block");
+			e.printStackTrace();
+			ExcelUtil.logExceptionInExcel(methodName, e.toString());
+			throw e;
+		}
 	}
-	
+
 	@And("Navigate to finance sales order page")
 	public void navigate_to_finance_sales_order_page() throws InterruptedException {
-	salesOrderPg.goToSaleOrderPg();
+		methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+
+		try {
+			salesOrderPg.goToSaleOrderPg();
+		}
+		catch(Exception e) {
+			System.out.println("In Catch Block");
+			e.printStackTrace();
+			ExcelUtil.logExceptionInExcel(methodName, e.toString());
+			throw e;
+		}
 	}
 
 	@And("User click on Campaign number")
 	public void user_click_on_campaign_number() throws InterruptedException {
-		
-		String CampID = testContext.Hmap.get("UTNumber");
-		System.out.println("camp id ="+CampID);
-		String CID [] = CampID.split("-");
-		String UTNumber = CID[0].trim();
-		//String UTNumber = UTN.trim();
-		System.out.println("UTNumber = "+UTNumber);
-		salesOrderPg.clickOnReceiptedCampaign(UTNumber);
-		
-		
-	
+		methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+
+		try {
+			String CampID = testContext.Hmap.get("UTNumber");
+			System.out.println("camp id ="+CampID);
+			String CID [] = CampID.split("-");
+			String UTNumber = CID[0].trim();
+			//String UTNumber = UTN.trim();
+			System.out.println("UTNumber = "+UTNumber);
+			salesOrderPg.clickOnReceiptedCampaign(UTNumber);
+		}
+		catch(Exception e) {
+			System.out.println("In Catch Block");
+			e.printStackTrace();
+			ExcelUtil.logExceptionInExcel(methodName, e.toString());
+			throw e;
+		}
+
+
 	}
 
 	@And("Select Campaign item number Checkbox and Click on Cancel Sales Order Lines")
 	public void select_campaign_item_number_checkbox_and_click_on_cancel_sales_order_lines() throws InterruptedException {
-		
-		
-		salesOrderPg.clickOnCheckBoxToCancelSOLines(testContext.Hmap.get("UTNumber"));
+		methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+
+
+		try {
+			salesOrderPg.clickOnCheckBoxToCancelSOLines(testContext.Hmap.get("UTNumber"));
+		}catch(Exception e) {
+			System.out.println("In Catch Block");
+			e.printStackTrace();
+			ExcelUtil.logExceptionInExcel(methodName, e.toString());
+			throw e;
+		}
 	}
 
 	@And("Select the Cancellation Reason and Fill the Reason")
 	public void select_the_cancellation_reason_and_fill_the_reason() {
-		salesOrderPg.SaleslineCancellationReason();
+		methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+		try {
+			salesOrderPg.SaleslineCancellationReason();
+		}catch(Exception e) {
+			System.out.println("In Catch Block");
+			e.printStackTrace();
+			ExcelUtil.logExceptionInExcel(methodName, e.toString());
+			throw e;
+		}
 	}
 
 	@And("Click on Confirm")
 	public void click_on_confirm() {
-		
-		salesOrderPg.SaleslineCancellationReasonConfirmation();
-	  
+		methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+
+		try {
+			salesOrderPg.SaleslineCancellationReasonConfirmation();
+		}
+		catch(Exception e) {
+			System.out.println("In Catch Block");
+			e.printStackTrace();
+			ExcelUtil.logExceptionInExcel(methodName, e.toString());
+			throw e;
+		}
 	}
 
 	@Then("Verify the sales order cancellation Popup")
 	public void verify_the_sales_order_cancellation_popup() throws InterruptedException {
-		salesOrderPg.VerifySOLineStatus(testContext.Hmap.get("UTNumber"));
-	    
+		methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+
+		try {
+			salesOrderPg.VerifySOLineStatus(testContext.Hmap.get("UTNumber"));
+		}
+		catch(Exception e) {
+			System.out.println("In Catch Block");
+			e.printStackTrace();
+			ExcelUtil.logExceptionInExcel(methodName, e.toString());
+			throw e;
+		}
 	}
 
 
