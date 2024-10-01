679e3eebfcfbc3a83aadbc71b0b1ed6a0103fb4b
chetan.patel <chetan.patel@host.docker.internal>
2024-10-01 20:28:56 +0530

    Commit

--- src/test/java/CommmonUtils/BaseAction.java
+++ src/test/java/CommmonUtils/BaseAction.java
@@ -65,58 +65,39 @@ public class BaseAction {
 
 	public void retryMechanism(WebDriver driver, WebElement ele) {
 
-		int maxAttempts = 4; // Maximum number of times you want to retry
+		try {
+			
+			wait.until(ExpectedConditions.visibilityOf(ele));
+			wait.until(ExpectedConditions.elementToBeClickable(ele));
+			ele.click();
 
-		int attempt = 1;
-		boolean elementClickable = false;
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-		while (attempt <= maxAttempts) {
-			try {
-				WebElement element = wait.until(ExpectedConditions.elementToBeClickable(ele));
-				element.click(); // Click the element once it becomes clickable
+		} catch (Exception e) {
 
-				elementClickable = true; // Set the flag to indicate it was clicked
+			e.printStackTrace();
+			throw e;
 
-				break; // Break the loop since the action was successful
-
-			} catch (Exception e) {
-				// Not clickable, maybe retry?
-
-				System.out.println("RETRY done " + attempt);
-				attempt++; // Increment the attempt count
-				e.printStackTrace();
-
-			}
 		}
-
 	}
+	
 
 	public void retryMechanismWithSendKeys(WebDriver driver, WebElement ele, String Key) {
 
-		int maxAttempts = 3; // Maximum number of times you want to retry
+try {
+			
+			wait.until(ExpectedConditions.visibilityOf(ele));
+			wait.until(ExpectedConditions.elementToBeClickable(ele));
+			ele.sendKeys(Key);;
 
-		int attempt = 1;
-		boolean elementClickable = false;
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-		while (attempt <= maxAttempts) {
-			try {
-				WebElement element = wait.until(ExpectedConditions.elementToBeClickable(ele));
-				element.sendKeys(Key); // Click the element once it becomes clickable
+		} catch (Exception e) {
 
-				elementClickable = true; // Set the flag to indicate it was clicked
+			e.printStackTrace();
+			throw e;
 
-				break; // Break the loop since the action was successful
-
-			} catch (Exception e) {
-				// Not clickable, maybe retry?
-
-				System.out.println("RETRY done " + attempt);
-				attempt++; // Increment the attempt count
-
-			}
 		}
 
 	}
+
+	
 
 	// method for table navigations amd text extraction
 	// input to this method is the xpath of the table till the tbody part
--- src/test/java/pageObjects/ZohoSignInPage.java
+++ src/test/java/pageObjects/ZohoSignInPage.java
@@ -4,13 +4,17 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ZohoSignInPage {
+import CommmonUtils.BaseAction;
+
+public class ZohoSignInPage{
 	
 	public WebDriver driver;
 	public WebDriverWait wait;
+	BaseAction ba; 
 	
     public final By emailId = By.xpath("//*[@id='login_id']");
     public final By nextButton = By.xpath("//*[@id='nextbtn']");
@@ -20,6 +24,7 @@ public class ZohoSignInPage {
     public ZohoSignInPage(WebDriver driver)
     {
     	this.driver = driver;
+    	ba=  new BaseAction (driver);
     }
     
     public void openZohoSignInPage(String url){
@@ -30,20 +35,28 @@ public class ZohoSignInPage {
 
     	wait = new WebDriverWait(driver,Duration.ofSeconds(20));
 
-    	wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(emailId)));
-
-    	driver.findElement(emailId).sendKeys(EMAIL);
+    //	wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(emailId)));
+    	
+ WebElement emailid = driver.findElement(By.xpath("//*[@id='login_id']"));
+    	//driver.findElement(emailId).sendKeys(EMAIL);
+    	ba.retryMechanismWithSendKeys(driver, emailid, EMAIL);
     }
     
     public void clickNextButton(){
-        driver.findElement(nextButton).click();
+    	WebElement NextButton = driver.findElement(By.xpath("//*[@id='nextbtn']"));
+        ba.retryMechanism(driver, NextButton);
     }
     
     public void enterPassword(String PASSWORD){
-        driver.findElement(passWord).sendKeys(PASSWORD);
+      //  driver.findElement(passWord).sendKeys(PASSWORD);
+        
+        WebElement EnterPass = driver.findElement(By.xpath("//*[@id='password']"));
+        ba.retryMechanismWithSendKeys(driver, EnterPass, PASSWORD);
     }
     
     public void clickSignInButton(){
-        driver.findElement(signInButton).click();
+        //driver.findElement(signInButton).click();
+    	WebElement SignInButton = driver.findElement(By.xpath("//*[@id='nextbtn']"));
+        ba.retryMechanism(driver, SignInButton);
     }
 }
